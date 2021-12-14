910c53067ebc93574a9b21818ea30cca4bf8eb37
Jaakko Junttila <jaakkojunttila@Jaakko-MacBook-Air.local>
2021-12-14 21:27:52 +0200

    14.12.21 piirakat toimii

--- src/Components/Piechart.js
+++ src/Components/Piechart.js
@@ -2,41 +2,37 @@ import '../App.css';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import React from 'react';
 import Plot from 'react-plotly.js';
+import React, { useState, useEffect } from 'react';
 
 export default function Piechart({ question }) {
 
-    const json = JSON.stringify(question.answers);
-    console.log(json)
-    const data = [{
-        values: [19, 26, 55],
-        labels: ['Residential', 'Non-Residential', 'Utility'],
-        type: 'pie'
-    }];
+    //const json = JSON.stringify(question.answers);
+    //console.log(json)
 
-    const layout = {
-        height: 400,
-        width: 500
-      };
+    const [values, setValues] = useState([]);
+    const [labels, setLabels] = useState([]);
+
+    useEffect(() => {
+        let num = []
+        const data = question.answers.map(a => a.answerText);
+        const unique = [...new Set(data)];
+        unique.forEach(text => num.push(data.filter(a => a === text).length))
+        setLabels(unique);
+        setValues(num)
+    }, [question])
 
     return (
         <>
-            <Plot
-            data={data}
-            layout={layout}
-            />
             <Card className='Cont'>  
                 <CardContent>
                     <Typography gutterBottom variant="body2" color="text.secondary">
                         {question.questionText}
                     </Typography>
-                    <br/>
-                    {question.answers.map((answer, i) => (
-                        <Typography gutterBottom  variant="body2" component="div" key={answer.answerId}>
-                            {i + 1}. {answer.answerText}
-                        </Typography>
-                    ))}
+                    <Plot
+                        data={[{ values: values, labels: labels, type: 'pie' }]}
+                        layout={{ height: 400, width: 500}}
+                    />
                 </CardContent>
             </Card>
         </>
